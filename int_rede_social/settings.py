9b4b5eab26a415b0e3b16a8940ba908b3cd90190
Adriano <adrianomarque135@gmail.com>
2025-08-03 01:30:21 -0300

    update_staticfiles

--- int_rede_social/settings.py
+++ int_rede_social/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'posts'
+    'posts',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 MIDDLEWARE = [
